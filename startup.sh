2de7ac0fa12420f65769adfbc46e3da6757eb2c6
flaviostutz <flaviostutz@gmail.com>
2019-07-25 09:56:55 -0300

    minor fix

--- startup.sh
+++ startup.sh
@@ -4,9 +4,9 @@ set -e
 
 echo "Starting Restic API..."
 backtor-restic \
-    --restic-password=$RESTIC_PASSWORD \
-    --log-level=$LOG_LEVEL \
-    --conductor-url=$CONDUCTOR_API_URL \
-    --repo-dir=$TARGET_DATA_PATH \
-    --source-path=$SOURCE_DATA_PATH
+    --restic-password="$RESTIC_PASSWORD" \
+    --log-level="$LOG_LEVEL" \
+    --conductor-url="$CONDUCTOR_API_URL" \
+    --repo-dir="$REPO_DIR" \
+    --source-path="$SOURCE_DATA_PATH"
 
